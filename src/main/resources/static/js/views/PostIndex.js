6a08f51856a8a6b27ca807b7835a3bafe29adfb6
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-16 17:44:57 -0400

    added DOM elements to display data from the API

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -5,7 +5,9 @@ export default function PostIndex(props) {
         </header>
         <main>
             <div id="posts-container">
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+                ${props.posts.map(post => `<h3>${post.title}</h3> <h3>${post.content}here</h3>`).join('')}
+             
+      
             </div>
         </main>
     `;
