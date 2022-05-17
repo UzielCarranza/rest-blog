e9f86627d49510dbf3deac77aef219241709e9c7
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-17 13:41:25 -0400

    completed edit function

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,5 +1,6 @@
-export default function PostIndex(props) {
+import createView from "../createView.js";
 
+export default function PostIndex(props) {
     // language=HTML
     return `
         <header>
@@ -7,8 +8,13 @@ export default function PostIndex(props) {
         </header>
         <main>
             <div id="posts-container">
-                ${props.posts.map(post => `<h3><button class="delete-post">Delete post</button> ${post.title}</h3> <h3 class="edit-post">${post.content}here</h3>
-                    <button class="edit-post">edit</button>
+                ${props.posts.map(post => ` <div class="container">
+            <h3 class="content-title">${post.title}</h3>
+            <h3 class="content-post">${post.content}</h3>
+            <span class="span-id" data-id="${post.id}"></span>
+                    <button class="btn edit-post">edit</button>
+                    <button class="btn delete-post">Delete post</button> 
+                    </div>
                 `).join('')}
 
             </div>
@@ -18,7 +24,7 @@ export default function PostIndex(props) {
             <input type="text" id="post-title" name="post-title">
             <label for="post-content">Content:</label>
             <input type="text" id="post-content" name="post-content">
-            <button id="create-form" value="submit" type="submit" ${PostEvent}>button</button>
+            <button id="create-post" value="submit" type="submit" ${PostEvent}>button</button>
         </form>
 
     `;
@@ -31,17 +37,71 @@ export function PostEvent() {
 }
 
 function postEventListener() {
-    $("#create-form").click(function () {
+    $("#create-post").click(function () {
         const content = $("#post-title").val();
         const title = $("#post-content").val();
-        console.log(content, title)
+        const newPost = {
+            title: `${title}`,
+            content: `${content}`
+
+        };
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newPost),
+        };
+        fetch("http://localhost:8080/api/posts", options)
+            .then(res => {
+                console.log(res.status);
+                createView("/posts")
+            }).catch(error => {
+            console.log(error);
+            createView("/posts");
+        });
     })
 }
 
 
 function editEventListener() {
     $(".edit-post").click(function () {
-        alert("edit")
+        let postIdToBeUpdate = $(this).parent().children('span').attr('data-id');
+
+        let postTitleToBeUpdate = $(this).parent().children('.content-title').html();
+
+        let postCommentToBeUpdate = $(this).parent().children('.content-post').html();
+        let title = prompt('enter new title');
+
+        let comment = prompt('enter new comment');
+
+        if (title) {
+            postTitleToBeUpdate = title;
+        }
+        if (comment) {
+            postCommentToBeUpdate = comment;
+        }
+        const updatedPost = {
+            id: `${postIdToBeUpdate}`,
+            title: `${postTitleToBeUpdate}`,
+            content: `${postCommentToBeUpdate}`
+        };
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedPost),
+        };
+        fetch(`http://localhost:8080/api/posts/${postIdToBeUpdate}`, options)
+            .then(res => {
+                console.log(res.status);
+                createView("/posts")
+            }).catch(error => {
+            console.log(error);
+            createView("/posts");
+        });
+
     })
 }
 
