b3dc8b9520c22ef3ec8983983ead0b8284ee0fe9
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-17 17:24:52 -0400

    added register link

--- src/main/java/com/example/restblog/web/ViewController.java
+++ src/main/java/com/example/restblog/web/ViewController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ViewController {
 
 
-    @RequestMapping({"/", "/about", "/posts", "/login", "/home"})
+    @RequestMapping({"/", "/about", "/posts", "/login", "/home", "/register"})
     public String showView() {
         return "forward:/index.html";
     }
