3c0e29b6dc3922ba2cb353590a332bed14cc3b2b
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-17 15:22:29 -0400

    created user class

---
+++ src/main/java/com/example/restblog/data/User.java
@@ -0,0 +1,66 @@
+package com.example.restblog.data;
+
+import java.util.Date;
+
+public class User {
+
+    private long id;
+    private String email;
+    private String password;
+    Date createdAt;
+    Role role;
+
+
+    public enum Role {USER, ADMIN}
+
+    public User(long id, String email, String password, Date createdAt, Role role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
