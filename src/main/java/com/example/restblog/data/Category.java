609a41762b51ed1f10b134716c414bf8fa315674
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-20 15:07:23 -0400

    commented out code to tests getting authors by posts

--- src/main/java/com/example/restblog/data/Category.java
+++ src/main/java/com/example/restblog/data/Category.java
@@ -1,46 +1,50 @@
-package com.example.restblog.data;
-
-import java.util.Collection;
-
-public class Category {
-
-    private Long id;
-    private String name;
-
-
-    // many to many relationship with Posts
-    private Collection<Post> posts;
-
-    public Category(Long id, String name, Collection<Post> posts) {
-        this.id = id;
-        this.name = name;
-        this.posts = posts;
-    }
-
-    public Category() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Collection<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(Collection<Post> posts) {
-        this.posts = posts;
-    }
-}
+//package com.example.restblog.data;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+//import java.util.Collection;
+//import java.util.List;
+//
+//public class Category {
+//
+//    private Long id;
+//    private String name;
+//
+//
+//    // many to many relationship with Posts
+//    @JsonIgnoreProperties("posts")
+//    private List<Post> posts;
+//
+//    public Category(Long id, String name, List<Post> posts) {
+//        this.id = id;
+//        this.name = name;
+//        this.posts = posts;
+//    }
+//
+//    public Category() {
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public List<Post> getPosts() {
+//        return posts;
+//    }
+//
+//    public void setPosts(List<Post> posts) {
+//        this.posts = posts;
+//    }
+//}
