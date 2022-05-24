9bf848d5caebf711fbabb6849322fa42cd9188d7
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-24 16:14:04 -0400

    set up categories repository

---
+++ src/main/java/com/example/restblog/data/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.restblog.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findCategoryByName(String category);
+}
