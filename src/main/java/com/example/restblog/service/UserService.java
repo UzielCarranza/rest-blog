c5164a1267913f3f562aa0ca63238b84722b1c66
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-24 11:37:44 -0400

    injected UsersRepository and PostsRepository into UserService class via constructor injection

--- src/main/java/com/example/restblog/service/UserService.java
+++ src/main/java/com/example/restblog/service/UserService.java
@@ -1,5 +1,18 @@
 package com.example.restblog.service;
 
+import com.example.restblog.data.PostsRepository;
+import com.example.restblog.data.UserRepository;
+
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PostsRepository postsRepository;
+
+    public UserService(UserRepository userRepository, PostsRepository postsRepository) {
+        this.userRepository = userRepository;
+        this.postsRepository = postsRepository;
+    }
+
+
 
 }
