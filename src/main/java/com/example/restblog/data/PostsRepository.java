f5655813b8b6bd704111ab5c0dc280cc75293638
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-24 16:25:33 -0400

    added method to look for a specific keyword in a query search

--- src/main/java/com/example/restblog/data/PostsRepository.java
+++ src/main/java/com/example/restblog/data/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.example.restblog.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Post, Long> {
 
-    void deleteById(Long id);
+    @Query("FROM Post a WHERE a.title like %:term%")
+    List<Post> searchByTiTleLike(@Param("term") String term);
 }
