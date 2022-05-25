6760d8cdd651d0569b6d0549ef2aeacd0929386b
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-25 12:16:40 -0400

    created function to reset form values when click

--- src/main/resources/static/js/views/Register.js
+++ src/main/resources/static/js/views/Register.js
@@ -11,40 +11,61 @@ export default function Register(props) {
         <title>Register Page</title>
     </head>
     <body>
-    <h1>Register with us</h1>
+    <section class="container d-flex flex-column justify-content-center">
+        <h1 class="text-center mt-4 mb-4">Register with us</h1>
 
-    <form id="Register-form" class="container">
-        <div class="form-group mb-2">
-            <label for="username">Username</label>
-            <input id="username" name="username" type="text" class="form-control" placeholder="Enter your username"/>
-        </div>
+        <form id="Register-form" class="container w-50 mt-4 mb-4">
+            <div class="form-group mb-4">
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" class="form-control"
+                       placeholder="Enter your username"/>
+            </div>
 
-        <div class="form-group mb-2">
-            <label for="email">Enter your email:</label>
-            <input pattern=".+@globex\\.com" size="30" type="email" id="email" name="email" class="form-control"
-                   placeholder="Enter your email" required>
-        </div>
+            <div class="form-group mb-4">
+                <label for="email">Enter your email:</label>
+                <input pattern=".+@globex\\.com" size="30" type="email" id="email" name="email" class="form-control"
+                       placeholder="Enter your email" required>
+            </div>
 
 
-        <div class="form-group mb-2">
-            <label for="password">Password</label>
-            <input id="password" name="password" type="password" autocomplete="on" class="form-control"
-                   placeholder="Enter a password"/>
-        </div>
-        <input id="register-btn" type="submit" value="Register" class="btn btn-primary mt-4"/>
-    </form>
+            <div class="form-group mb-4">
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" autocomplete="on" class="form-control"
+                       placeholder="Enter a password"/>
+            </div>
+            <div class="container mt-2 mb-2 p-0">
+                <input id="register-btn" type="submit" value="Register" class="btn btn-primary mr-4"/>
+                <input id="reset-btn" type="reset" value="reset" class="btn btn-primary ml-4"/>
+            </div>
+        </form>
+    </section>
     </body>
     </html>`;
 
 }
 
 export function RegisterEvent() {
+    registerButton();
+    resetButton();
+}
+
+
+const registerButton = () => {
     $('#register-btn').click(function () {
-        fetchAction("POST",{
+        fetchAction("POST", {
             username: $("#username").val(),
             email: $("#email").val(),
             password: $("#password").val(),
 
-        },"http://localhost:8080/api/users", "/" )
+        }, "http://localhost:8080/api/users", "/")
     });
+}
+
+const resetButton = () => {
+    $('#reset-btn').click(function () {
+        console.log("click")
+        $("#username").val("");
+        $("#email").val("")
+        $("#password").val("")
+    })
 }
